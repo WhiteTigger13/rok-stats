4a84daa64ba44be3b9314379405cf8fb4c15b0f4
WhiteTigger13 <deperne@yahoo.com>
2024-11-20 21:23:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const datasets = [
         { name: "Dataset 1", file: "data/dataset1.csv", uploadDate: "2024-11-20" },
         { name: "Dataset 2", file: "data/dataset2.csv", uploadDate: "2024-11-20" }
-         { name: "Dataset 3", file: "data/dataset3a.csv", uploadDate: "2024-11-20" }
+       
     ];
 
     // Populate dropdown menu with datasets
